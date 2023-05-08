619474424ecd913604b4443d68fa34fa5f76dceb
Brian Yao <newbeachfound@gmail.com>
2023-05-07 21:30:27 -0500

    added provider

--- flutter_and_firebase/lib/main.dart
+++ flutter_and_firebase/lib/main.dart
@@ -1,9 +1,9 @@
-// ignore_for_file: avoid_print
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_and_firebase/firebase_options.dart';
 import './screens/wrapper.dart';
+import 'package:flutter_and_firebase/services/auth.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,8 +17,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
-    );
+    return StreamProvider.value(
+        initialData: null,
+        value: AuthService().user,
+        child: MaterialApp(
+          home: Wrapper(),
+        ));
   }
 }
